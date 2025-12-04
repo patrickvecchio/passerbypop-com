eb8001e189810a45389ba9bb41106e9a5ee84142
Patrick R. Vecchio <28739368+patrickvecchio@users.noreply.github.com>
2025-12-04 02:41:09 -0800

    Feature/site layout (#1)
    * site layout plus music, video links, and social media.
    
    * fixed prerender errors and update Node version
    
    ---------
    
    Co-authored-by: Patrick R. Vecchio <patrickv@bsgindustries.com>

---
+++ web/src/lib/components/Footer.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import PlayButton from "$lib/components/PlayButton.svelte";
+
+</script>
+
+<div class="flex justify-between">
+    <div>
+        <div class="flex grow gap-2">
+            <img class="" src="/images/passerby-thumbnail.jpg" alt="Passerby Album Cover" />
+            <div class="">
+                <div class="mb-2">An All New Romance</div>
+                <div class="mb-2">
+                    <PlayButton size={32} fill="white" />
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="">
+        <form class="block">
+            <input
+                type="email"
+                placeholder="Your email"
+                class="rounded-xl px-4 py-2"
+            />
+            <button
+                type="submit"
+                class="btn-icon preset-filled text-sm" title="Join"
+            >
+                Join
+            </button>
+        </form>
+    </div>
+</div>
---
+++ web/src/routes/music/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<div class="w-[600px] h-[516px] bg-[url('/images/3d-bucking-bronco-transparent.png')] bg-center bg-no-repeat align-center content-center">
+	<div class="flex grow justify-around items-center">
+		<a href="https://open.spotify.com/artist/4Suodx3L3va4gro9PZYLpx?si=2zeLhsabRL2HF2jYeMPAcg" target="_blank" rel="noopener noreferrer">
+            <img src="/images/spotify-logo.png" alt="Spotify Logo">
+        </a>
+        <a href="https://geo.itunes.apple.com/us/artist/passerby/128870710?mt=1&app=music&itscg=30200&itsct=afftoolset_1" target="_blank" rel="noopener noreferrer">
+            <img src="/images/itunes-logo.png" alt="Apple Music Logo">
+        </a>
+        <a href="https://play.google.com/store/music/artist/Passerby?id=A5atdjynmcuv6qeaqvsuctpfaci&hl=en" target="_blank" rel="noopener noreferrer">
+            <img src="/images/google-play-logo.png" alt="Google Play Logo">
+        </a>
+	</div>
+</div>
